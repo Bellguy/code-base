064ff5fe86267b9f5d3a0912b0e0e297407266c4
Bellguy <120140259+Bellguy@users.noreply.github.com>
2023-02-07 19:18:50 -0800

    Finished tuples exercise

--- Rust/tuples.rs
+++ Rust/tuples.rs
@@ -5,25 +5,23 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    let coll = iter;
-    
-    // TODO: Finish implementing
-    /*
-    for (index, T) in coll.enumerate() {
-        if index % 2 == 0 {
-            
-        }
-    */
+    let even_nums = iter.enumerate()
+        .filter(|(i,v)| i % 2 == 0)
+        .map(|(i,v)| v);
+    return even_nums
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        // TODO
-        unimplemented!("implement `fn manhattan`")
+       (&self.0 - 0).abs() + (&self.1 - 0).abs()
     }
 }
 
 fn main() {
-    
+    let my_div = divmod(450, 79);
+    let my_pos = Position(89,430).manhattan();
+
+    println!("{:?}", my_div);
+    println!("{:?}", my_pos);
 }
